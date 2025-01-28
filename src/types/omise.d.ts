19f35e1e3cc44225ad9a7340bcc3d362c28c8b5d
Veerachat Petpisit <Veerachat_p@outlook.com>
2025-01-28 09:48:05 +0700

    Omise fix

--- src/types/omise.d.ts
+++ src/types/omise.d.ts
@@ -1,42 +1,43 @@
 declare module 'omise' {
   interface OmiseStatic {
-    (config: { publicKey: string; secretKey: string }): OmiseInstance;
+    (config: { publicKey: string; secretKey: string }): Omise;
   }
 
-  interface OmiseInstance {
+  interface Omise {
     charges: {
       create(data: {
         amount: number;
         currency: string;
         card?: string;
-        source?: string;
         customer?: string;
         description?: string;
-        metadata?: Record<string, any>;
-        capture?: boolean;
       }): Promise<Charge>;
+      retrieve(id: string): Promise<Charge>;
+    };
+    customers: {
+      create(data: {
+        email: string;
+        description?: string;
+        card?: string;
+      }): Promise<Customer>;
     };
   }
 
   interface Charge {
     id: string;
-    status: 'successful' | 'pending' | 'failed';
     amount: number;
     currency: string;
+    status: string;
+    customer: string;
+  }
+
+  interface Customer {
+    id: string;
+    email: string;
     description: string;
-    metadata: Record<string, any>;
-    card?: any;
-    source?: any;
-    customer?: string;
-    failure_code?: string;
-    failure_message?: string;
-    paid: boolean;
-    transaction: string;
-    refunded: number;
-    refunds: any;
-    created: string;
+    cards: any;
   }
 
   const omise: OmiseStatic;
-  export default omise;
+  export = omise;
 } 
